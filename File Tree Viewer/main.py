2d5379ab47498430b26e26d9b66b1d242e730982
Calinescu Mihai <69072635+CMihai99@users.noreply.github.com>
2021-04-13 23:16:42 +0300

    Create main.py

---
+++ File Tree Viewer/main.py
@@ -0,0 +1,42 @@
+'''
+Developed by: Calinescu Mihai
+Date: Feb, 2021
+
+Github: https://github.com/CMihai99
+'''
+
+# Import Required Packages
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QTreeView, QFileSystemModel, QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QModelIndex
+
+# Main Window
+class FileView(QWidget):
+	def __init__(self, dir_path):
+		super().__init__()
+		# Setup Window
+		self.setFixedSize(700, 300)
+		self.setWindowTitle('File Viewer')
+		self.setWindowIcon(QIcon("icon.png"))
+		self.setGeometry(640, 360, 700, 300)
+		self.model = QFileSystemModel()
+		self.model.setRootPath(dir_path)
+		self.tree =  QTreeView()
+		self.tree.setModel(self.model)
+		self.tree.setRootIndex(self.model.index(dirPath))
+		self.tree.setColumnWidth(0, 250)
+		self.tree.setAlternatingRowColors(True)
+
+		# Layout
+		layout = QVBoxLayout()
+		layout.addWidget(self.tree)
+		self.setLayout(layout)
+
+# Run App
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	dirPath = r'<Your directory>'
+	demo = FileView(dirPath)
+	demo.show()
+	sys.exit(app.exec_())
