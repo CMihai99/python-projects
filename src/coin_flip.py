01567b59e3a5bb698e46b50e6698446853c99560
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-02 11:14:27 +0300

    Update coin_flip.py
    Update coin_flip.py
    
    Update signature at the top
    Import time module to wait 2 seconds for the flip
    Add error handling if response is no or none of the above
    Clean-up code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/coin_flip.py
+++ src/coin_flip.py
@@ -1,22 +1,28 @@
-'''
-Developed by: Calinescu Mihai
-Date: 8 Apr, 2021
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 2nd, 2021
 
-Github: https://github.com/CMihai99
-'''
+import random, time
 
+heads_choice = "heads"
+tails_choice = "tails"
 
-import random
+flip_again = "yes"
 
-user_input = int(input("Enter number of times to flip coin: "))
+# Flip the coin
+while flip_again == "yes" or flip_again == "Yes" or flip_again == "y" or flip_again == "Y":
+    print("Flipping the coin..")
 
-coin_choices = ('heads', 'tails')
-coin_flips = []
+    time.sleep(2)
 
-for i in range(user_input): # FLips coin x number of times depending on user input
-    coin_flip = random.choice(coin_choices)
-    coin_flips.append(coin_flip)
+    print("Flipped the coin. Result is", [heads_choice, tails_choice][random.randint(0,1)])
 
-print(coin_flips)
-print(coin_flips.count('tails'), "tails")
-print(coin_flips.count('heads'), "heads")
+    # FLip the coin again
+    flip_again = input("Flip again (y/n)? ")
+
+# If response is no, stop flipping
+while flip_again == "no" or flip_again == "No" or flip_again == "n" or flip_again == "N":
+    print("Flipping stopped.")
+
+# If response is none of the above, stop program
+else:
+    print("Invalid response.")
