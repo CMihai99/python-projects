f38111fc205da91e40d6b83cbae7c1ccb22bd06f
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-02 11:15:25 +0300

    Update dice_roll.py
    Update dice_roll.py
    
    Update signature at the top
    Update waiting for the roll to 3 seconds
    Add error handling if response is no or none of the above
    Clean-up code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/dice_roll.py
+++ src/dice_roll.py
@@ -1,27 +1,28 @@
-'''
-Developed by: Calinescu Mihai
-Date: 8 Apr, 2021
-
-Github: https://github.com/CMihai99
-'''
-
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 2nd, 2021
 
 import random, time
 
-# Assign variables
 min = 1
 max = 6
 
 roll_again = "yes"
 
 # Roll the dices
-while roll_again == "yes" or roll_again == "y" or roll_again == "Y" or roll_again == "Yes":
-    print("Rolling the dices...")
-    time.sleep(1)
-    print("Following values are:", random.randint(min, max), "and", random.randint(min, max))
+while roll_again == "yes" or roll_again == "Yes" or roll_again == "y" or roll_again == "Y":
+    print("Rolling the dices..")
+
+    time.sleep(3)
+
+    print("Rolled the dices. Values are", random.randint(min, max), "and", random.randint(min, max))
 
     # Roll the dices again
-    roll_again = input("Roll again? ")
+    roll_again = input("Roll again (y/n)? ")
 
+# If response is no, stop flipping
+while flip_again == "no" or flip_again == "No" or flip_again == "n" or flip_again == "N":
+    print("Rolling stopped.")
+
+# If response is none of the above, stop program
 else:
-    print("Process finished")
+    print("Invalid response.")
