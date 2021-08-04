bcefb8e475c53824a28a620c6424ea27f57397c4
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-04 12:32:32 +0300

    Remove ability to decrypt string
    Remove ability to decrypt string
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

---
+++ src/encrypt_string.py
@@ -0,0 +1,16 @@
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 3rd, 2021
+
+from cryptography.fernet import Fernet
+
+string = input("Enter string: ")
+
+# Generate encrypting key
+key = Fernet.generate_key()
+
+# Fernet key class
+key_class = Fernet(key)
+
+# Encrypt string
+encrypt_message = key_class.encrypt(string.encode())
+print("Encrypted string:", encrypt_message)
