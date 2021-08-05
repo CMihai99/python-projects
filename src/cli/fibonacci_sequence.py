40465628294e7555719983ffedf9435a052947f4
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-05 15:02:40 +0300

    Cleanup fibonacci_sequence.py code
    Cleanup fibonacci_sequence.py code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/fibonacci_sequence.py
+++ src/cli/fibonacci_sequence.py
@@ -1,27 +1,25 @@
-'''
-Developed by: Calinescu Mihai
-Date: 18 Mar, 2021
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 5th, 2021
 
-Github: https://github.com/CMihai99
-'''
+number = int(input("Enter number: "))
 
-
-# Calculate sequence
 def fibonacci(n):
-    # Declare variables
     a = 0
     b = 1
-    
-    # Initialize sequence
+
     if n == 1:
         print(a)
+
     else:
         print(a)
         print(b)
+
         for i in range(2, n):
             c = a + b
             a = b
             b = c
             print(c)
 
-fibonacci(int(input("Enter number: ")))
+print("Fibonacci sequence:")
+
+fibonacci(number)
