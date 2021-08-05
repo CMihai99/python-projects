a81a0959f7024153029e7568032698476b59ae5d
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-05 15:02:55 +0300

    Cleanup fizzbuzz.py code
    Cleanup fizzbuzz.py code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/fizzbuzz.py
+++ src/cli/fizzbuzz.py
@@ -1,19 +1,20 @@
-'''
-Developed by: Calinescu Mihai
-Year: 2021
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 5th, 2021
 
-Github: https://github.com/CMihai99
-'''
-
-
-for fizzbuzz in range(int(input("Max Range: "))):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0: # Check fizzbuzz
+for fizzbuzz in range(int(input("Range: "))):
+    # 0 prints fizzbuzz
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print("fizzbuzz")
         continue
+
+    # Multiples of 3 print fizz
     elif fizzbuzz % 3 == 0:
         print("fizz")
         continue
+
+    # Multiples of 5 print buzz
     elif fizzbuzz % 5 == 0:
         print("buzz")
         continue
+
     print(fizzbuzz)
