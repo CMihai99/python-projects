60546b479f4d9244be0863d844d4da5d793fbc89
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-07 11:42:59 +0300

    Cleanup `madlibs.py` code
    Cleanup `madlibs.py` code

--- src/cli/madlibs.py
+++ src/cli/madlibs.py
@@ -1,17 +1,11 @@
-'''
-Developed by: Calinescu Mihai
-Date: 8 Apr, 2021
-
-Github: https://github.com/CMihai99
-'''
-
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 7th, 2021
 
 adjective = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous Person: ")
+verb1 = input("First verb: ")
+verb2 = input("Second verb: ")
+famous_person = input("Famous person: ")
 
-# Concatenate Strings
-madlib = f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+madlibs = f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
-print(madlib)
+print(madlibs)
