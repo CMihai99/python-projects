3ce1a44ca9bb7c192d13cff64452f42d29783e2a
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-07 12:58:16 +0300

    Format answer and remove ability to flip again
    Format answer and remove ability to flip again
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/coin_flip.py
+++ src/cli/coin_flip.py
@@ -6,23 +6,13 @@ import random, time
 heads_choice = "heads"
 tails_choice = "tails"
 
+flip_result = [heads_choice, tails_choice][random.randint(0, 1)]
 flip_again = "yes"
 
 # Flip the coin
-while flip_again == "yes" or flip_again == "Yes" or flip_again == "y" or flip_again == "Y":
+if flip_again == "yes" or flip_again == "Yes" or flip_again == "y" or flip_again == "Y":
     print("Flipping the coin..")
 
     time.sleep(2)
 
-    print("Flipped the coin. Result is", [heads_choice, tails_choice][random.randint(0, 1)])
-
-    # FLip the coin again
-    flip_again = input("Flip again (y/n)? ")
-
-# If response is no, stop flipping
-while flip_again == "no" or flip_again == "No" or flip_again == "n" or flip_again == "N":
-    print("Flipping stopped.")
-
-# If response is none of the above, stop program
-else:
-    print("Invalid response.")
+    print(f"Flipped the coin. Result is {flip_result}")
