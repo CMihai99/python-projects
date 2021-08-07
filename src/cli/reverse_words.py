14568ce8427506c786dfbf024a083896707f343b
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-07 11:38:35 +0300

    Cleanup `reverse_words.py` code
    Cleanup `reverse_words.py` code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

---
+++ src/cli/reverse_words.py
@@ -0,0 +1,8 @@
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 7th, 2021
+
+words = input("Enter words: ")
+
+reversed_words = words[::-1]
+
+print("Reversed words:", reversed_words)
