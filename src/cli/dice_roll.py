1da28bc83f10f5209901adca139cf675bb622251
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-07 12:58:20 +0300

    Format answer and remove ability to roll again
    Format answer and remove ability to roll again
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/dice_roll.py
+++ src/cli/dice_roll.py
@@ -1,28 +1,18 @@
 # Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
-# Date: August 4th, 2021
+# Date: August 7th, 2021
 
 import random, time
 
 min = 1
 max = 6
 
+roll_result = random.randint(min, max)
 roll_again = "yes"
 
-# Roll the dices
-while roll_again == "yes" or roll_again == "Yes" or roll_again == "y" or roll_again == "Y":
-    print("Rolling the dices..")
+# Roll the dice
+if roll_again == "yes" or roll_again == "Yes" or roll_again == "y" or roll_again == "Y":
+    print("Rolling the dice..")
 
-    time.sleep(3)
+    time.sleep(2)
 
-    print("Rolled the dices. Values are", random.randint(min, max), "and", random.randint(min, max))
-
-    # Roll the dices again
-    roll_again = input("Roll again (y/n)? ")
-
-# If response is no, stop flipping
-while flip_again == "no" or flip_again == "No" or flip_again == "n" or flip_again == "N":
-    print("Rolling stopped.")
-
-# If response is none of the above, stop program
-else:
-    print("Invalid response.")
+    print(f"Rolled the dice. Result is {roll_result}")
