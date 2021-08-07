4969e2bad12e984f2754eac2d8b309830b0625ef
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-07 11:43:47 +0300

    Cleanup `madlibs.py` code
    Cleanup madlibs.py code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/madlibs.py
+++ src/cli/madlibs.py
@@ -6,6 +6,6 @@ verb1 = input("First verb: ")
 verb2 = input("Second verb: ")
 famous_person = input("Famous person: ")
 
-madlibs = f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+madlib = f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
-print(madlibs)
+print(madlib)
