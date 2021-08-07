92fc7a34338b84cafd6e07db1c070644d0f95632
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-07 11:37:11 +0300

    Update `reverse_sentence.py` naming conventions
    Update reverse_sentence.py naming conventions
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/reverse_sentence.py
+++ src/cli/reverse_sentence.py
@@ -1,11 +1,11 @@
 # Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
-# Date: August 5th, 2021
+# Date: August 7th, 2021
 
-string = input("Enter sentence: ")
+sentence = input("Enter sentence: ")
 
-def reverse_sentence(sentence):
-    words = sentence.split(' ')  # Split string into words
+def reverse(sentence):
+    words = sentence.split(' ') # Split string into words
     reverse_sentence = ' '.join(reversed(words)) # Reverse split string and join using space  
-    return reverse_sentence  # Return joined string
+    return reverse_sentence # Return joined string
 
-print("Reversed sentence:", reverse_sentence(string))
+print("Reversed sentence:", reverse(sentence))
