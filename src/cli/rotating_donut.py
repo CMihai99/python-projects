91707503b40017085f0bd2da4d22af975221f7e5
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-09 14:25:32 +0300

    Cleanup code
    Cleanup code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/rotating_donut.py
+++ src/cli/rotating_donut.py
@@ -1,24 +1,24 @@
-'''
-Developed by: Calinescu Mihai
-Date: 8 Apr, 2021
-
-Github: https://github.com/CMihai99
-'''
-
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 9th, 2021
 
 from vpython import *
 
-canvas(background = color.white) # Change background color
+canvas(background = color.white, width = 1520, height = 705)
 
 base = ring(radius = 0.55, thickness = 0.35, color = vector(400, 100, 1))
+
 topping = ring(radius = 0.85, thickness = 0.35, color = vector(0.4, 0.2, 0))
 
-rad = 0 # Rotate donut
+# Rotation
+rad = 0
 
 while True:
-    rate(30) # Change rotation speed
+    # Rotation speed
+    rate(30)
 
-    # Change rotation axis
+    # Rotation axis
     base.pos = vector(3*sin(rad), cos(rad), 1)
+
     topping.pos = vector(3*sin(rad), cos(rad), 1)
+
     rad = rad + 0.05
