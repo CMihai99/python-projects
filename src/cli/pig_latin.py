780b3cd9b305bba0359c7ac96070cd72da3b9408
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-09 14:04:40 +0300

    Cleanup code
    Cleanup code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/pig_latin.py
+++ src/cli/pig_latin.py
@@ -1,40 +1,34 @@
-'''
-Developed by: Calinescu Mihai
-Date: 16 Mar, 2021
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 9th, 2021
 
-Github: https://github.com/CMihai99
-'''
+def translate():
+    characters = ['sh', 'gl', 'ch', 'ph', 'tr', 'br', 'fr', 'bl', 'gr', 'st', 'sl', 'cl', 'pl', 'fl']
 
+    sentence = input('Enter sentence: ')
+    sentence = sentence.split()
 
-import time
+    # Translate sentence to pig latin
+    for k in range(len(sentence)):
+        i = sentence[k]
 
-def main():
-        characters = ['sh', 'gl', 'ch', 'ph', 'tr', 'br', 'fr', 'bl', 'gr', 'st', 'sl', 'cl', 'pl', 'fl']
+        if i[0] in ['a', 'e', 'i', 'o', 'u']:
+            sentence[k] = i+'ay'
 
-        # Input
-        sentence = input('Enter sentence: ')
-        sentence = sentence.split()
-        time.sleep(0.5)
-        print("Translating to Pig Latin...")
-        time.sleep(0.8)
+        elif t(i) in characters:
+            sentence[k] = i[2:]+i[:2]+'ay'
 
-        # Translate sentence into pig latin
-        for k in range(len(sentence)):
-                i = sentence[k]
-                if i[0] in ['a', 'e', 'i', 'o', 'u']:
-                        sentence[k] = i+'ay'
-                elif t(i) in characters:
-                        sentence[k] = i[2:]+i[:2]+'ay'
-                elif i.isalpha() == False:
-                        sentence[k] = i
-                else:
-                        sentence[k] = i[1:]+i[0]+'ay'
-        return ' '.join(sentence)
+        elif i.isalpha() == False:
+            sentence[k] = i
+
+        else:
+            sentence[k] = i[1:]+i[0]+'ay'
+
+    return ' '.join(sentence)
 
 def t(str):
-        return str[0]+str[1]
+    return str[0]+str[1]
 
-# Run
 if __name__ == "__main__":
-        x = main()
-        print(x)
+    x = translate()
+
+    print(f"Pig latin translation: {x}")
