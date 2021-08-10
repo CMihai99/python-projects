325341aa03369730f434939254cfca50f14ac684
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-10 12:32:00 +0300

    Cleanup code
    Cleanup code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/rock_paper_scissors.py
+++ src/cli/rock_paper_scissors.py
@@ -1,15 +1,10 @@
-'''
-Developed by: Calinescu Mihai
-Date: 8 Apr, 2021
-
-Github: https://github.com/CMihai99
-'''
-
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 10th, 2021
 
 import random
 from enum import IntEnum
 
-class Action(IntEnum): # List actions
+class Action(IntEnum):
     Rock = 0
     Paper = 1
     Scissors = 2
@@ -20,40 +15,47 @@ victories = {
     Action.Rock: [Action.Scissors],
 }
 
-def get_user_selection():
+def user_selection():
     choices = [f"{action.name} ({action.value})" for action in Action]
     choices_str = ", ".join(choices)
-    selection = int(input(f"Enter a choice - {choices_str}: "))
+
+    selection = int(input(f"Enter choice: {choices_str}: "))
+
     action = Action(selection)
+
     return action
 
 def get_computer_selection():
     selection = random.randint(0, len(Action) - 1)
+
     action = Action(selection)
+
     return action
 
 def determine_winner(user_action, computer_action):
     defeats = victories[user_action]
+
     if user_action == computer_action:
-        print(f"Both players selected {user_action.name}. It's a tie!")
+        print(f"Both players selected {user_action.name}. Tie.")
+
     elif computer_action in defeats:
-        print(f"{user_action.name} beats {computer_action.name}! You win!")
+        print(f"{user_action.name} beats {computer_action.name}! You win.")
+
     else:
         print(f"{computer_action.name} beats {user_action.name}! You lose.")
 
 # Invalid response
 while True:
     try:
-        user_action = get_user_selection()
+        user_action = user_selection()
+
     except ValueError as e:
         range_str = f"[0, {len(Action) - 1}]"
-        print(f"Invalid selection. Enter a value in range {range_str}")
+
+        print("Invalid selection.")
+
         continue
 
     computer_action = get_computer_selection()
-    determine_winner(user_action, computer_action)
 
-    # Play again
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
-        break
+    determine_winner(user_action, computer_action)
