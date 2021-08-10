566b969d83f3cfd6c12c8a28ab1ea6239d87198f
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-08-10 12:45:27 +0300

    Cleanup code
    Cleanup code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/tip_calculator.py
+++ src/cli/tip_calculator.py
@@ -1,24 +1,12 @@
-'''
-Developed by: Calinescu Mihai
-Date: 14 Mar, 2021
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: August 10th, 2021
 
-Github: https://github.com/CMihai99
-'''
-
-
-# Assign values to the variables
-meal = int(input("Input meal price: "))
-tax = 0.0675
+meal = float(input("Enter meal price: "))
 tip = 0.15
 
-# Calculate total values
-total_meal = meal + meal * tax
 total_tip = meal * tip
-total_tipnotax = meal + meal * tip
-total_tipwithtax = total_meal + meal * tip
+total_price = meal + meal * tip
 
-# Print result
 print("Meal: %.2f" % meal)
 print("Tip: %.2f" % total_tip)
-print("Total without tax: %.2f" % total_tipnotax)
-print("Total with tax: %.2f" % total_tipwithtax)
+print("Total: %.2f" % total_price)
