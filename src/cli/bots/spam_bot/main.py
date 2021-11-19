e1a2afc0a5d244cee946aa76eccc6cef547957dc
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-11-19 17:13:02 +0200

    Cleanup `main.py` code
    Cleanup main.py code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/bots/spam_bot/main.py
+++ src/cli/bots/spam_bot/main.py
@@ -1,16 +1,12 @@
-'''
-Developed by: Calinescu Mihai
-Date: 18 Jan, 2021
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: November 19th, 2021
 
-Github: https://github.com/CMihai99
-'''
+import pyautogui
 
+document = open("DOCUMENT.TXT", 'r')
 
-import pyautogui, time
-
-time.sleep(0.5)
-f = open("loremipsum.txt", 'r') # Select document
-
-for word in f:
-    pyautogui.typewrite(word)   # Type the document selected line by line
-    pyautogui.press("enter")    # And press enter so send the message
+for word in document:
+    # Type selected document
+    pyautogui.typewrite(word)
+    # Send the message
+    pyautogui.press("enter")
