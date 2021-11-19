6426d86463abffbe0945ee8963a34b25f3c56851
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-11-19 16:49:37 +0200

    Fix mistake
    Fix mistake
    
    Removed irrelevant code due to copy-pasting wrong text
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/text_adventure_games/scenario_1.py
+++ src/cli/text_adventure_games/scenario_1.py
@@ -1,20 +1,6 @@
 # Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
 # Date: November 19th, 2021
 
-import pyttsx3, PyPDF2
-
-document = open('oop.pdf', 'rb')
-documentReader = PyPDF2.PdfFileReader(document)
-pages = documentReader.numPages
-speaker = pyttsx3.init()
-
-# Read document
-for num in range(0, pages):
-    page = documentReader.getPage(7)
-    text = page.extractText()
-    speaker.say(text)
-    speaker.runAndWait()
-
 import time
 
 answer = input("Would you like to play? ")
