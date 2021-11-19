5f2699f680ac2fae123fc140338d3a5e91f36f1d
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-11-19 16:30:24 +0200

    Cleanup `weatherscrape.py` code
    Cleanup weatherscrape.py code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/web_scraping/weather.gov/weatherscrape.py
+++ src/cli/web_scraping/weather.gov/weatherscrape.py
@@ -1,23 +1,16 @@
-'''
-Developed by: Calinescu Mihai
-Date: Feb, 2021
-
-Github: https://github.com/CMihai99
-'''
-
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: November 19th, 2021
 
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# Get Information
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=34.09927000000005&lon=-118.33806999999996#.XIRJ81NKgUE')
 soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
 
 items = week.find_all(class_='tombstone-container')
 
-# Print Information
 print(items[0].find(class_='period-name').get_text())
 print(items[0].find(class_='short-desc').get_text())
 print(items[0].find(class_='temp').get_text())
@@ -27,14 +20,14 @@ short_descriptions = [item.find(class_='short-desc').get_text() for item in item
 temperatures = [item.find(class_='temp').get_text() for item in items]
 print(period_names)
 
-# Setup Information
-weather_stuff = pd.DataFrame(
+weather_info = pd.DataFrame(
     {
         'period': period_names,
         'short_descriptions': short_descriptions,
         'temperatures': temperatures,
-     })
-print(weather_stuff)
+    })
+
+print(weather_info)
 
 # Convert To CSV
-weather_stuff.to_csv('weather.csv')
+weather_info.to_csv('weather.csv')
