010071427111a8daaede17f4a1c238a87a64e806
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-11-19 17:39:57 +0200

    Cleanup `audio.py` code
    Cleanup audio.py code
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/cli/captcha/audio.py
+++ src/cli/captcha/audio.py
@@ -1,17 +1,10 @@
-'''
-Developed by: Calinescu Mihai
-Date: 8 Apr, 2021
-
-Github: https://github.com/CMihai99
-'''
-
+# Developed by: Calinescu Mihai <mihaimihaia431@gmail.com>
+# Date: November 19th, 2021
 
 from captcha.audio import AudioCaptcha
 
-audio = AudioCaptcha() # Stores in an audio file
+user_input = input("Enter text to store as audio (only numbers): ")
+data = audio.generate(user_input)
 
-user_input = input("Enter text to store as audio (only numbers): ") # Input
-
-data = audio.generate(user_input) # Generates data
-
-audio.write(user_input, 'audio/{}.wav'.format(user_input)) # Writes file with dynamic filename
+audio = AudioCaptcha()
+audio.write(user_input, 'audio/{}.wav'.format(user_input))
